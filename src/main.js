cd350ffb9f3096ce77f281a5cbcb536a450d3ff1
SpooKy <spookyhammer2300@gmail.com>
2022-08-30 19:05:32 +0530

    array of objects sorting by wins test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const records = require("./resources").RECORDS;
 const Clan = require("./database_models/clan").Clan;
 
+
+var standings = []
 for(let clanRecord of records){
     var clan = new Clan(clanRecord);
     var message = `
@@ -14,5 +16,21 @@ for(let clanRecord of records){
     starsAgainst => ${clan.starsAgainst}
     Avg S.D => ${((clan.starsFor - clan.starsAgainst)/clan.matchesPlayed).toFixed(2)}
     `
-    console.log(message);
+    // console.log(message);
+    standings.push(clan)
 }
+
+sortedByWins = standings.sort((a,b) => {
+    return a.wins - b.wins;
+})
+console.log(standings)
+console.log("_------------------")
+console.log(sortedByWins)
+
+// for(let i of standings){
+    // i.averageSD = ((i.starsFor - i.starsAgainst)/i.matchesPlayed)
+    // // console.log(i)
+// }
+
+
+// console.log(standings);
