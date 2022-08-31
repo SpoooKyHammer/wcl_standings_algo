7e10eaf4f55e95090e9a26157791f851e4cf11c2
SpooKy <spookyhammer2300@gmail.com>
2022-08-31 13:13:15 +0530

    added more objects

--- src/resources.js
+++ src/resources.js
@@ -1,11 +1,11 @@
 const RECORDS = [
     {
-        abb: "ABC",
-        clanTag: "#ABC",
+        abb: "AOW",
+        clanTag: "#RYRQU2UG",
         opponent: {
                     WK1: {
-                            abb: "XYZ",
-                            clanTag: "#XYZ",
+                            abb: "CU",
+                            clanTag: "#V8YCJJ9G",
                             status: "W",
                             starFor: 14,
                             starAgainst: 11,
@@ -28,19 +28,18 @@ const RECORDS = [
                         starFor: null,
                         starAgainst: null,
                         perDest: null
-                    
                 },
                     
         },
         conference: "ARCHER"
   },
   {
-        abb: "XYZ",
-        clanTag: "#XYZ",
+        abb: "CU",
+        clanTag: "#V8YCJJ9G",
         opponent: {
                     WK1: {
-                            abb: "ABC",
-                            clanTag: "#ABC",
+                            abb: "AOW",
+                            clanTag: "#RYRQU2UG",
                             status: "W",
                             starFor: 14,
                             starAgainst: 12,
@@ -58,8 +57,82 @@ const RECORDS = [
                             starAgainst: 15,
                             perDest: 99.90
                     },
+                    WK4: {
+                        status: "W",
+                        starFor: null,
+                        starAgainst: null,
+                        perDest: null
+                },
         },
         conference: "ARCHER"
+  },
+  {
+        abb: "LP4",
+        clanTag: "#88V088P",
+        opponent: {
+                    WK1: {
+                            abb: "SLY",
+                            clanTag: "#UJG8U9UR",
+                            status: "W",
+                            starFor: 12,
+                            starAgainst: 11,
+                            perDest: 90.11
+                    },
+                    WK2: {
+                            status: "L",
+                            starFor: 10,
+                            starAgainst: 14,
+                            perDest: 80.20
+                    },
+                    WK3: {
+                            status: "T",
+                            starFor: 14,
+                            starAgainst: 14,
+                            perDest: 90.50
+                    },
+                    WK4: {
+                        status: "L",
+                        starFor: null,
+                        starAgainst: null,
+                        perDest: null
+                },
+                    
+        },
+        conference: "GIANT"
+  },
+  {
+        abb: "SLY",
+        clanTag: "#UJG8U9UR",
+        opponent: {
+                    WK1: {
+                            abb: "LP4",
+                            clanTag: "#88V088P",
+                            status: "L",
+                            starFor: 10,
+                            starAgainst: 11,
+                            perDest: 90.11
+                    },
+                    WK2: {
+                            status: "L",
+                            starFor: 10,
+                            starAgainst: 14,
+                            perDest: 80.20
+                    },
+                    WK3: {
+                            status: "W",
+                            starFor: 15,
+                            starAgainst: 14,
+                            perDest: 100
+                    },
+                    WK4: {
+                        status: "W",
+                        starFor: null,
+                        starAgainst: null,
+                        perDest: null
+                },
+                    
+        },
+        conference: "GIANT"
   }
 ];
 
